02b30d381c203e0294010dc5b50b9bfeda6653fd
nojebar <nicolas.ojeda.bar@lexifi.com>
2022-06-29 07:40:58 +0200

    maps

--- test.ml
+++ test.ml
@@ -1,26 +1,106 @@
 open! Core
 open Core_bench
 
-let[@tail_mod_cons] rec init_tailrec_aux i n f =
-  if i >= n then []
-  else f i :: init_tailrec_aux (i+1) n f
 
-let init len f =
-  if len < 0 then invalid_arg "List.init" else
-  init_tailrec_aux 0 len f
+let[@tail_mod_cons] rec map f = function
+    [] -> []
+  | [a1] ->
+      let r1 = f a1 in
+      [r1]
+  | [a1; a2] ->
+      let r1 = f a1 in
+      let r2 = f a2 in
+      [r1; r2]
+  | [a1; a2; a3] ->
+      let r1 = f a1 in
+      let r2 = f a2 in
+      let r3 = f a3 in
+      [r1; r2; r3]
+  | a1::a2::a3::l ->
+      let r1 = f a1 in
+      let r2 = f a2 in
+      let r3 = f a3 in
+      r1 :: r2 :: r3 :: map f l
+
+let[@tail_mod_cons] rec mapi i f = function
+    [] -> []
+  | [a1] ->
+      let r1 = f i a1 in
+      [r1]
+  | [a1; a2] ->
+      let r1 = f i a1 in
+      let r2 = f (i+1) a2 in
+      [r1; r2]
+  | [a1; a2; a3] ->
+      let r1 = f i a1 in
+      let r2 = f (i+1) a2 in
+      let r3 = f (i+2) a3 in
+      [r1; r2; r3]
+  | a1::a2::a3::l ->
+      let r1 = f i a1 in
+      let r2 = f (i+1) a2 in
+      let r3 = f (i+2) a3 in
+      r1::r2::r3::mapi (i+3) f l
+
+let mapi f l = mapi 0 f l
+
+let[@tail_mod_cons] rec map2 f l1 l2 =
+  match (l1, l2) with
+    ([], []) -> []
+  | ([a1], [b1]) ->
+      let r1 = f a1 b1 in
+      [r1]
+  | ([a1; a2], [b1; b2]) ->
+      let r1 = f a1 b1 in
+      let r2 = f a2 b2 in
+      [r1; r2]
+  | ([a1; a2; a3], [b1; b2; b3]) ->
+      let r1 = f a1 b1 in
+      let r2 = f a2 b2 in
+      let r3 = f a3 b3 in
+      [r1; r2; r3]
+  | (a1::a2::a3::l1, b1::b2::b3::l2) ->
+      let r1 = f a1 b1 in
+      let r2 = f a2 b2 in
+      let r3 = f a3 b3 in
+      r1::r2::r3::map2 f l1 l2
+  | (_, _) -> invalid_arg "List.map2"
 
 let g = ref []
 
-let test n =
+let map2_test n =
+  let lst = Stdlib.List.init n Fun.id in
   Command_unix.run
     (Bench.make_command
       [
-        Bench.Test.create ~name:(Printf.sprintf "  trmc %d" n) (fun () -> g := init n Fun.id);
-        Bench.Test.create ~name:(Printf.sprintf "stdlib %d" n) (fun () -> g := Stdlib.List.init n Fun.id);
+        Bench.Test.create ~name:(Printf.sprintf "map2 trmc   %d" n) (fun () -> g := map2 (+) lst lst);
+        Bench.Test.create ~name:(Printf.sprintf "map2 stdlib %d" n) (fun () -> g := Stdlib.List.map2 (+) lst lst);
       ]
     )
 
-let cases = [(*1; 2; 3; 4; 5; 6; 7; 8; 9; 10; *)100; 1000; 10000; 100000]
+let mapi_test n =
+  let lst = Stdlib.List.init n Fun.id in
+  Command_unix.run
+    (Bench.make_command
+      [
+        Bench.Test.create ~name:(Printf.sprintf "mapi trmc   %d" n) (fun () -> g := mapi (+) lst);
+        Bench.Test.create ~name:(Printf.sprintf "mapi stdlib %d" n) (fun () -> g := Stdlib.List.mapi (+) lst);
+      ]
+    )
+
+let map_test n =
+  let lst = Stdlib.List.init n Fun.id in
+  Command_unix.run
+    (Bench.make_command
+      [
+        Bench.Test.create ~name:(Printf.sprintf "map trmc   %d" n) (fun () -> g := map succ lst);
+        Bench.Test.create ~name:(Printf.sprintf "map stdlib %d" n) (fun () -> g := Stdlib.List.map succ lst);
+      ]
+    )
+
+let cases = [1; 2; 3; 4; 5; 6; 7; 8; 9; 10; 100; 1000; 10000; 100000; (*1000000*)]
 
 let () =
-  Stdlib.List.iter test cases
+  Stdlib.List.iter map2_test cases;
+  Stdlib.List.iter mapi_test cases
+  Stdlib.List.iter map_test cases

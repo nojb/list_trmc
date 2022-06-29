a68d2eaa9dd02aa0dc66db3a46c8c528c18f2121
nojebar <nicolas.ojeda.bar@lexifi.com>
2022-06-29 07:08:11 +0200

    List.init

--- test.ml
+++ test.ml
@@ -1,43 +1,26 @@
 open! Core
 open Core_bench
 
-let[@tail_mod_cons] rec trmc_map f = function
-  | [] -> []
-  | [x1] ->
-    let y1 = f x1 in
-    [y1]
-  | [x1; x2] ->
-    let y1 = f x1 in
-    let y2 = f x2 in
-    [y1; y2]
-  | [x1; x2; x3] ->
-    let y1 = f x1 in
-    let y2 = f x2 in
-    let y3 = f x3 in
-    [y1; y2; y3]
-  | x1 :: x2 :: x3 :: xs ->
-    let y1 = f x1 in
-    let y2 = f x2 in
-    let y3 = f x3 in
-    y1 :: y2 :: y3 :: trmc_map f xs
+let[@tail_mod_cons] rec init_tailrec_aux i n f =
+  if i >= n then []
+  else f i :: init_tailrec_aux (i+1) n f
 
-let rec map f = function
-  | [] -> []
-  | x :: xs -> let y = f x in y :: map f xs
+let init len f =
+  if len < 0 then invalid_arg "List.init" else
+  init_tailrec_aux 0 len f
 
 let g = ref []
 
 let test n =
-  let l = Stdlib.List.init n Fun.id in
   Command_unix.run
     (Bench.make_command
       [
-        Bench.Test.create ~name:(Printf.sprintf "  trmc %d" n) (fun () -> g := trmc_map Fun.id l);
-        Bench.Test.create ~name:(Printf.sprintf "stdlib %d" n) (fun () -> g := map Fun.id l);
+        Bench.Test.create ~name:(Printf.sprintf "  trmc %d" n) (fun () -> g := init n Fun.id);
+        Bench.Test.create ~name:(Printf.sprintf "stdlib %d" n) (fun () -> g := Stdlib.List.init n Fun.id);
       ]
     )
 
-let cases = [1; 2; 3; 4; 5; 6; 7; 8; 9; 10; 100; 1000; 10000; 100000]
+let cases = [(*1; 2; 3; 4; 5; 6; 7; 8; 9; 10; *)100; 1000; 10000; 100000]
 
 let () =
   Stdlib.List.iter test cases
